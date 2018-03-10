8998df2d56c7f48b3ac980c659a2ade2333c8e77
jeliashi <jeliashi@ucsd.edu>
2018-03-09 19:45:59 -0800

    Started creating the mess

---
+++ app/src/main/java/com/jeliav/android/rtaandnoise/view/BaseSurface.java
@@ -0,0 +1,71 @@
+package com.jeliav.android.rtaandnoise.view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.support.annotation.NonNull;
+import android.util.AttributeSet;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.Function;
+
+/**
+ * Created by jeliashiv on 3/9/18.
+ */
+
+abstract class BaseSurface extends SurfaceView {
+    public AtomicBoolean active = new AtomicBoolean(false);
+
+    public BaseSurface(Context context){
+        super(context);
+        getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder surfaceHolder) {
+                active.set(true);
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+                active.set(false);
+
+            }
+        });
+    }
+
+    public BaseSurface(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder surfaceHolder) {
+                active.set(true);
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+                active.set(false);
+
+            }
+        });
+    }
+
+
+    public void drawSurface(Function<Canvas,Canvas> ifActive){
+        if (this.active.get()){
+            SurfaceHolder  holder = this.getHolder();
+            Canvas oldCanvas = holder.lockCanvas();
+            holder.unlockCanvasAndPost(ifActive.apply(oldCanvas));
+        }
+    }
+}
